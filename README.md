4c8c8cb8caa5ec179efdbb217335c0a8212fd396
Rasheeda Alexander <rasheedaalexander@Rasheedas-MBP.fios-router.home>
2019-03-05 19:35:56 -0500

    Updated README. Display all of Blue line's train locations after user click

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # next-stop
+A simple web application that displays when the next train will arrive based on User's selected location & train line.
--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 const params = config.api_key;
 
+function getTrains() {
+// document.getElementById('rd').addEventListener('click', getData);
+
 fetch('https://api.wmata.com/StationPrediction.svc/json/GetPrediction/All?api_key=' + params)
-.then(function(response) {
+.then(response => {
   if (!response.ok) {
     throw Error(response.statusText);
   }
-  // Read response as json
   return response.json();
 })
-.then(function(responseAsJson) {
-  const myJSON = JSON.stringify(responseAsJson);
-  document.getElementsByClassName('one')[0].innerHTML = JSON.stringify(responseAsJson.Trains[0]);
-  console.log(responseAsJson.Trains[0]);
+.then(myJson => {
+  // const myJson = JSON.stringify(myJson.Trains);
+  // const RD = document.getElementById('rd').id;
+  // const OR = document.getElementById('or').id;
+  // const BL = document.getElementById('bl').id;
+  // const SL = document.getElementById('sl').id;
+  // const GR = document.getElementById('gr').id;
+  // const YL = document.getElementById('yl').id;
+  // const colors = [RD, OR, BL, SL, GR, YL];
+
+document.getElementsByClassName('one')[0].innerHTML = JSON.stringify(myJson.Trains[0]);
+  console.log(myJson);
+
+  let i, x = "";
+
+  for (i in myJson.Trains) {
+    if (myJson.Trains[i].Line === 'BL') {
+  x += JSON.stringify(myJson.Trains[i].LocationName) + "<br>";
+  document.getElementById("ok").innerHTML = x;
+}
+}
+
+
 })
-.catch(function(error) {
+.catch(error => {
   console.log('Looks like there was a problem: \n', error);
 });
+}
+
+const abc = document.querySelector('.line');
+abc.addEventListener('click', getTrains);
+// ******************************************************************
+
+// function myFunction() {
+//   document.getElementById('rd').innerHTML = 'HEY!';
+//
+//   console.log(Object.values(myJson));
+// }
+//
+// console.log(response.json());
